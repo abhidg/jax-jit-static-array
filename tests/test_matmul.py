98da14348f52612f2adf9014facdff9ee4b9391a
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2025-09-23 15:30:03 +0100

    Use numpy.testing.assert_array_equal

--- tests/test_matmul.py
+++ tests/test_matmul.py
@@ -1,9 +1,9 @@
 import numpy as np
+import numpy.testing as npt
 
 import jax.random as jrand
 import jax.numpy as jnp
 from jax_jit_static_array.matmul import matmul_jit, matmul_nonjit
-import numpy as np
 
 key = jrand.key(0)
 x = jnp.arange(1, 7, dtype=np.float32)
@@ -12,8 +12,8 @@ expected = np.array([5, 11, 17])
 
 def test_matmul_nonjit():
     y = matmul_nonjit(x, mul)
-    assert (y[0] == expected).all()
+    npt.assert_array_equal(y[0], expected)
 
 def test_matmul_jit():
     y = matmul_jit(x, mul)
-    assert (y[0] == expected).all()
+    npt.assert_array_equal(y[0], expected)
