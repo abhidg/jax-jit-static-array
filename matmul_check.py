b5b7d62ec45025c5b64998a1fae1d1b4789b2b16
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2025-09-23 11:05:44 +0100

    More compact output

--- matmul_check.py
+++ matmul_check.py
@@ -1,27 +1,38 @@
-from jax_jit_static_array.matmul import matmul_jit, matmul_nonjit
 import numpy as np
-import jax.random as jrand
-import jax.numpy as jnp
 import time
 import sys
+
+import jax.random as jrand
+import jax.numpy as jnp
+from jax_jit_static_array.matmul import matmul_jit, matmul_nonjit
+
 key = jrand.key(0)
 x = jnp.arange(1, 7, dtype=np.float32)
 mul = (2.0, 3.0, 1.0, 2.0)
 
-print("Non-JIT version:")
-t_nonjit = time.time()
+sys.stdout.write("nonjit ")
+sys.stdout.flush()
+
+t_nonjit_start = time.time()
 for i in range(100_000):
     matmul_nonjit(x + jrand.normal(key, x.shape), mul)
     if i % 1000 == 0:
         sys.stdout.write(".")
         sys.stdout.flush()
-print("\nTime elapsed (non-JIT): ", time.time() - t_nonjit)
+t_nonjit = time.time() - t_nonjit_start
+print(f"\nnonjit {t_nonjit:.2f}s")
 
-t_jit = time.time()
-print("JIT version:")
+t_jit_start = time.time()
+sys.stdout.write("   jit ")
+sys.stdout.flush()
 for i in range(100_000):
     matmul_jit(x + jrand.normal(key, x.shape), mul)
     if i % 1000 == 0:
         sys.stdout.write(".")
         sys.stdout.flush()
-print("\nTime elapsed (JIT): ", time.time() - t_jit)
+t_jit = time.time() - t_jit_start
+print(f"\n   jit {t_jit:.2f}s")
+
+if t_jit > t_nonjit:
+    print("JIT is usually faster, so this is being considered a failure")
+    sys.exit(1)
